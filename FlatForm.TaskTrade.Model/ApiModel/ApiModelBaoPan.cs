ca09c84311a47787156959e2a025b6bf446a7e91
黄英龙 <lyjgz@21cn.com>
2016-07-18 16:42:34 +0800

    Delete

--- FlatForm.TaskTrade.Model/ApiModel/ApiModelBaoPan.cs
+++
@@ -1,9 +0,0 @@
-﻿using GHSoft.DTO2;
-
-namespace Peacock.PEP.Model.ApiModel
-{
-    public class ApiModelBaoPan : HouseOfferForSaleDTO
-    {
-        public string SourceLink { get; set; }
-    }
-}
--- FlatForm.TaskTrade.Model/ApiModel/ApiModelResidentialArea.cs
+++
@@ -1,9 +0,0 @@
-﻿using GHSoft.DTO2;
-
-namespace Peacock.PEP.Model.ApiModel
-{
-    public class ApiModelResidentialArea : ResidentialAreaDTO
-    {
-        public long ResidentialAreaID { get; set; }
-    }
-}
